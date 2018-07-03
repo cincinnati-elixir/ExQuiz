569bc62f54ac049a481a53c1d5e916399ca0a4be
James Smith <st23am@gmail.com>
2018-07-03 19:42:16 -0400

    Initial Elixir App

---
+++ lib/ex_quizet.ex
@@ -0,0 +1,18 @@
+defmodule ExQuizet do
+  @moduledoc """
+  Documentation for ExQuizet.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ExQuizet.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ex_quizet_test.exs
@@ -0,0 +1,8 @@
+defmodule ExQuizetTest do
+  use ExUnit.Case
+  doctest ExQuizet
+
+  test "greets the world" do
+    assert ExQuizet.hello() == :world
+  end
+end
